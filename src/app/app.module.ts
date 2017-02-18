0bed1328f12a73950b4648b2946aae39fd9d75c5
TJ Lavelle <tlavelle@google.com>
2017-02-18 17:26:38 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { InvitationComponent } from './invitation/invitation.component';
+import { HeroComponent } from './hero/hero.component';
+import { CountDownComponent } from './count-down/count-down.component';
+
+const appRoutes: Routes = [
+  { path: 'invitation', component: InvitationComponent },
+  { path: '', redirectTo: '/invitation', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvitationComponent,
+    HeroComponent,
+    CountDownComponent,
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule.forRoot(),
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/count-down/count-down.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MdIconRegistry } from '@angular/material';
+
+
+@Component({
+  selector: 'app-count-down',
+  templateUrl: './count-down.component.html',
+  styleUrls: ['./count-down.component.css']
+})
+export class CountDownComponent implements OnInit {
+
+  eventDate: Date
+  seconds: number
+  minutes: number
+  hours: number
+  days: number
+
+  constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'bride-and-groom',
+      sanitizer.bypassSecurityTrustResourceUrl('/assets/ico-bride-groom.svg'));
+  }
+
+  ngOnInit() {
+    this.eventDate = new Date('Sat, 22 Jul 2017 17:00:00 EST');
+    setInterval(() => {
+      this.updateCountDown();
+    }, 60);
+  }
+
+  updateCountDown() {
+    const _second = 1000;
+    const _minute = _second * 60;
+    const _hour = _minute * 60;
+    const _day = _hour * 24;
+    const nowDate = Date.now();
+    const diff = this.eventDate.getTime() - nowDate;
+
+    this.days = Math.floor(diff / _day);
+    this.hours = Math.floor((diff % _day) / _hour);
+    this.minutes = Math.floor((diff % _hour) / _minute);
+    this.seconds = Math.floor((diff % _minute) / _second);
+  }
+
+
+
+}
