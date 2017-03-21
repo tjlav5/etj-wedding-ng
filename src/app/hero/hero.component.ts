9f3e4785a5db1a6923e7a36c56cde099fb11679f
TJ Lavelle <tlavelle@google.com>
2017-03-20 22:39:45 -0400

    update

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MdIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent implements OnInit {
 
-  constructor() { }
+  constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('logo', sanitizer.bypassSecurityTrustResourceUrl('/assets/etj-logo.svg'));
+  }
 
   ngOnInit() {
   }
