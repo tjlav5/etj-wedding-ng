621596deca2682eebd766d4aee2784efe65d4015
TJ Lavelle <tlavelle@google.com>
2017-03-25 15:41:14 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ViewChild } from '@angular/core/src/metadata/di';
 import { MdDialog, MdDialogRef } from '@angular/material';
 import { NavigationComponent } from './navigation/navigation.component';
-
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +11,12 @@ import { NavigationComponent } from './navigation/navigation.component';
 })
 export class AppComponent {
 
-  constructor(public dialog: MdDialog) {}
-
+  constructor(public dialog: MdDialog, public sanitizer: DomSanitizer) {
+  }
+  
   headerColor = '#FFFFFF';
   headerBackground = 'rgba(256, 256, 256, 0.0)';
+  boxShadow = this.sanitizer.bypassSecurityTrustStyle('0 0 6px 1px rgba(0, 0, 0, 0)');
 
   onPageYChange(pageY:number) {
     const MAX = 300;
@@ -23,6 +25,7 @@ export class AppComponent {
     const color = Math.floor(256.0 - (opacity * 256.0));
     this.headerBackground = `rgba(256, 256, 256, ${opacity})`;
     this.headerColor = `rgb(${color}, ${color}, ${color})`;
+    this.boxShadow = this.sanitizer.bypassSecurityTrustStyle(`0 0 6px 1px rgba(0, 0, 0, ${opacity})`);
   }
 
   openDialog() {
