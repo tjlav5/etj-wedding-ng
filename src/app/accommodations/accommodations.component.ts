ff22369111e5375ee84e9edac037cd2053d99ba3
TJ Lavelle <tlavelle@google.com>
2017-03-28 23:13:18 -0400

    updates

--- src/app/accommodations/accommodations.component.ts
+++ src/app/accommodations/accommodations.component.ts
@@ -1,3 +1,5 @@
+declare var google: any;
+
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MdIconRegistry } from '@angular/material';
@@ -9,10 +11,6 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./accommodations.component.scss']
 })
 export class AccommodationsComponent implements OnInit {
-
-  disableDefaultUI: boolean = true
-  lat: number = 51.678418;
-  lng: number = 7.809007;
 
   src: string = `url("https://maps.googleapis.com/maps/api/staticmap?key=${environment.mapKey}&center=40.751280,-73.937111&zoom=14&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0xf5f5f5&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x616161&style=element:labels.text.stroke%7Ccolor:0xf5f5f5&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.business%7Cvisibility:off&style=feature:poi.park%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:poi.park%7Celement:labels.text%7Cvisibility:off&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:road%7Celement:geometry%7Ccolor:0x9f9f9f&style=feature:road.arterial%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:road.highway%7Celement:geometry%7Ccolor:0x7a7a7a&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:transit.line%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:transit.station%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:water%7Celement:geometry%7Ccolor:0x1a1818&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&size=1000x1000&markers=icon:https://goo.gl/843HfH%7C40.74976,-73.948978&markers=icon:https://goo.gl/LOHZh8%7C40.752807, -73.927283")`;
 
@@ -28,6 +26,194 @@ export class AccommodationsComponent implements OnInit {
   }
 
   ngOnInit() {
+    const iconBase = 'https://etj.wedding/assets';
+    const icons = {
+      heart: `${iconBase}/ico-venue.png`,
+      hotel: `${iconBase}/ico-hotel-blue.png`,
+    };
+    const style = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#7a7a7a"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#1a1818"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+];
+    const latLng = {
+      longIslandCity: {lat: 40.751280, lng: -73.937111},
+      metropolitanBuilding: {lat: 40.74976, lng: -73.948978},
+      paperFactory: {lat: 40.752807, lng: -73.927283}
+    };
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: latLng.longIslandCity,
+      disableDefaultUI: true,
+      fullscreenControl: true,
+      gestureHandling: 'cooperative',
+      styles: style,
+      zoom: 14,
+    });
+    const hotelMarker = new google.maps.Marker({
+      icon: icons.hotel,
+      position: latLng.paperFactory,
+      map: map
+    });
+    const venueMarker = new google.maps.Marker({
+      icon: icons.heart,
+      position: latLng.metropolitanBuilding,
+      map: map
+    });
   }
 
 }
